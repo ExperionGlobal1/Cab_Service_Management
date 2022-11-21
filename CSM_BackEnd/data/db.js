45b10541c731e093876ed0193c271d3778907f0c
Raj Kishore yadav <115794376+ExperionGlobal1@users.noreply.github.com>
2022-11-21 16:43:52 +0530

    Add files via upload

---
+++ CSM_BackEnd/data/db.js
@@ -0,0 +1,23 @@
+const mysql =require('mysql2');
+
+const connectionDetails ={
+    host:'localhost',
+    user:'root',
+    password:'Experion@123',
+    database:'cab'
+}
+
+function getConnection (){
+    return mysql.createConnection(connectionDetails);
+}
+
+function executeQuery(query,parameters,callback){
+    let connection = getConnection();
+    connection.connect();
+    connection.query(query,parameters,callback);
+    connection.commit;
+    connection.end();
+
+}
+
+module.exports.executeQuery=executeQuery;
