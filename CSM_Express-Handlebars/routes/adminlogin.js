037f5f3d8e0599a9611a08acdd758e3e79514e96
Raj Kishore yadav <115794376+ExperionGlobal1@users.noreply.github.com>
2022-12-05 15:40:35 +0530

    Add files via upload

---
+++ CSM_Express-Handlebars/routes/adminlogin.js
@@ -0,0 +1,24 @@
+
+const express =require('express');
+const cc = require('../controllers/adminloginController');
+const sign = require('../controllers/adminController');
+
+
+const router = express.Router()
+
+
+router.get('/alogin', cc.login);
+router.post('/alogin', cc.loginPost);
+router.get('/aregister', sign.create);
+router.post('/aregister', sign.createPost);
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
---
+++ CSM_Express-Handlebars/routes/driverlogin.js
@@ -0,0 +1,24 @@
+
+const express =require('express');
+const cc = require('../controllers/driverloginController');
+const sign = require('../controllers/driverController');
+
+
+const router = express.Router()
+
+
+router.get('/dlogin', cc.login);
+router.post('/dlogin', cc.loginPost);
+router.get('/dregister', sign.create);
+router.post('/dregisterpost', sign.createPost);
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
